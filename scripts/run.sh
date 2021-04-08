071e1ca4fd4b11e65ca69874c577cc2c8c926e4f
QuangPham <quangg2012@gmail.com>
2021-04-08 18:12:19 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 
-PREFIX="--save_path results/ --batch_size 10 --data_path data/ --cuda yes --n_epochs 1 --use 1.0 --model bcl_dual --temperature 5 --memory_strength 100"
-CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file mnist_permutations.pt --lr 0.03 --beta 0.3 --inner_steps 1 --n_meta 2 --n_memories 256
-CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file cifar-cl.pt --lr 0.3 --beta 0.1 --inner_steps 2 --n_meta 1 --n_memories 65
-CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file cub-cl.pt --lr 0.03 --beta 0.3 --inner_steps 1 --n_meta 2 --n_memories 50
-CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file mini-cl.pt --lr 0.05 --beta 0.1 --inner_steps 2 --n_meta 1 --n_memories 65
+PREFIX="--save_path results/ --batch_size 10 --data_path data/ --cuda yes --n_epochs 1 --use 1.0 --model ctn --temperature 5 --memory_strength 100 --lr 0.01 --beta 0.05"
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file mnist_permutations.pt --inner_steps 2 --n_meta 2 --n_memories 50 --use 1000
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file cifar-cl.pt --inner_steps 2 --n_meta 2 --n_memories 50
+CUDA_VISIBLE_DEVICES=0 python main.py $PREFIX --data_file mini-cl.pt  --inner_steps 2 --n_meta 2 --n_memories 50
